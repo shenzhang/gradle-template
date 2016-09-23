77bce91a5730cfc21c79bf856dc86bd2f922641e
Zhang Shen <shenzhang722@gmail.com>
2016-09-23 22:36:49 +0800

    Remove web-app related files

---
+++ src/main/groovy/GroovyMain.groovy
@@ -0,0 +1,10 @@
+/**
+ * User: shenzhang
+ * Date: 6/3/14
+ * Time: 10:56 PM
+ */
+class GroovyMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
